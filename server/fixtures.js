a1cf3ce4a52b00c89cc8c154a923e31aac9a87d6
Brano Susta <susta@lansystems.sk>
2016-02-09 20:58:18 +0100

    Routing to a single post page.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,16 @@
 if (Posts.find().count() === 0) {
-    Posts.insert({
+  Posts.insert({
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
-    });
+  });
 
-Posts.insert({
+  Posts.insert({
     title: 'Meteor',
     url: 'http://meteor.com'
-});
+  });
 
-    Posts.insert({
+  Posts.insert({
     title: 'The Meteor Book',
     url: 'http://themeteorbook.com'
-});
+  });
 }
